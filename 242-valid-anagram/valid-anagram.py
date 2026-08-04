4de22606b032fa95b4b2e87385325339b5d84495
shakkeera T <shakkeerawindow@gmail.com>
2026-08-04 14:34:09 +0530

    Time: 15 ms (44.40%) | Memory: 19.5 MB (30.05%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        f1={}
+        f2={}
+        for i in s:
+            if i in f1:
+                f1[i]+=1
+            else:
+                f1[i]=1
+        for i in t:
+            if i in f2:
+                f2[i]+=1
+            else:
+                f2[i]=1        
+
+        return (f1==f2)    
